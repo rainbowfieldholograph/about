a082ac2b7ef49cf1ce50ee23213510e9fa74d6bd
youthfulanger <prorock-666@inbox.ru>
2021-09-29 16:24:15 +0900

    some fixes..

--- src/components/myLink/MyLink.jsx
+++ src/components/myLink/MyLink.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const MyLink = ({ href, icon }) => {
   return (
-    <a href={href} target="_blank">
+    <a rel="noreferrer" href={href} target="_blank">
       <FontAwesomeIcon color="white" icon={icon} />
     </a>
   )
