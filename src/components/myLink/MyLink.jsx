deebb8904f0c03a8363d44babfebeda4ed7246a1
youthfulanger <prorock-666@inbox.ru>
2021-10-02 12:23:11 +0900

    added animation

--- src/components/myLink/MyLink.jsx
+++ src/components/myLink/MyLink.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import styles from './MyLink.module.css'
 
 const MyLink = ({ href, icon }) => {
   return (
     <a rel="noreferrer" href={href} target="_blank">
-      <FontAwesomeIcon color="white" icon={icon} />
+      <FontAwesomeIcon className={styles.icon} color="white" icon={icon} />
     </a>
   )
 }
