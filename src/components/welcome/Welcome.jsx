903e074e7425ed3d99385e405f2937f351145e1c
rainbowfieldholograph <somebleed@tutanota.com>
2022-01-30 21:05:10 +0900

    update welcome component

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -6,9 +6,9 @@ const Welcome = () => {
     <section className={styles.wrapper}>
       <div className={styles.inner}>
         <div>
-          <h1 className={styles.text}>Привет</h1>
+          <p className={styles.text}>Привет</p>
           <p className={styles.text}>Я являюсь программистом-веб разработчиком.</p>
-          <p className={styles.text}>Данное приложение - это моё небольшое портфолио.</p>
+          <p className={styles.text}>Данное приложение - моё небольшое портфолио.</p>
         </div>
       </div>
     </section>
