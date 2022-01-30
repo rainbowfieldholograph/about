e80fc11c3370669b15a7a0a07f046d4920e39073
rainbowfieldholograph <somebleed@tutanota.com>
2022-01-30 12:51:22 +0900

    optimized pngs

--- src/appData/worksData/WorksData.js
+++ src/appData/worksData/WorksData.js
@@ -1,45 +1,45 @@
-import eyesClosedImg from '../../img/eyesclosed.png'
-import maybetomorrowImg from '../../img/maybetomorrow.png'
-import moodboardImg from '../../img/moodboard.png'
-import reactsneakersImg from '../../img/react-sneakers.png'
+// import eyesClosedImg from '../../img/eyesclosed.png'
+// import maybetomorrowImg from '../../img/maybetomorrow.png'
+// import moodboardImg from '../../img/moodboard.png'
+// import reactsneakersImg from '../../img/react-sneakers.png'
 import waterTransportation from '../../img/water-transportation.png'
 
 export const worksData = [
-  {
-    image: eyesClosedImg,
-    title: 'Eyes-closed',
-    descr:
-      'Fullstack приложение для прослушивания и загрузки музыки. Выполнен деплой на heroku',
-    stack: 'React, MongoDB, NodeJS, Express, GraphQL, ApolloServer, ApolloCilent, s3 buckets',
-    project: 'https://eyes-closed.herokuapp.com/',
-    github: 'https://github.com/rainbowfieldholograph/music-playlist-fullstack',
-  },
-  {
-    image: maybetomorrowImg,
-    title: 'Maybetomorrow',
-    descr:
-      'Улучшенная версия Todo-list. Является fullstack приложением, поддерживается возможность авторизации пользователей',
-    stack: 'React, NodeJS, MongoDB, Express, GraphQL, ApolloCilent, JWT',
-    project: 'https://maybetomorrow.herokuapp.com/',
-    github: 'https://github.com/rainbowfieldholograph/todo-client-server',
-  },
-  {
-    image: moodboardImg,
-    title: 'Moodboard',
-    descr: 'Demo версия старой работы на React(+ добавил минимальный функционал)',
-    stack: 'React',
-    project: 'https://rainbowfieldholograph.github.io/moodboard-old-work/',
-    github: 'https://github.com/rainbowfieldholograph/moodboard-old-work',
-  },
-  {
-    image: reactsneakersImg,
-    title: 'React sneakers',
-    descr:
-      'Приложение, сделанное по видеокурсу react sneakers от archakov. Исправлено много багов в изначальном коде (до сих пор много багов, по-тихоньку исправляю)',
-    stack: 'React, MockAPI',
-    project: 'https://rainbowfieldholograph.github.io/react-sneakers/',
-    github: 'https://github.com/rainbowfieldholograph/react-sneakers',
-  },
+  // {
+  //   image: eyesClosedImg,
+  //   title: 'Eyes-closed',
+  //   descr:
+  //     'Fullstack приложение для прослушивания и загрузки музыки. Выполнен деплой на heroku',
+  //   stack: 'React, MongoDB, NodeJS, Express, GraphQL, ApolloServer, ApolloCilent, s3 buckets',
+  //   project: 'https://eyes-closed.herokuapp.com/',
+  //   github: 'https://github.com/rainbowfieldholograph/music-playlist-fullstack',
+  // },
+  // {
+  //   image: maybetomorrowImg,
+  //   title: 'Maybetomorrow',
+  //   descr:
+  //     'Улучшенная версия Todo-list. Является fullstack приложением, поддерживается возможность авторизации пользователей',
+  //   stack: 'React, NodeJS, MongoDB, Express, GraphQL, ApolloCilent, JWT',
+  //   project: 'https://maybetomorrow.herokuapp.com/',
+  //   github: 'https://github.com/rainbowfieldholograph/todo-client-server',
+  // },
+  // {
+  //   image: moodboardImg,
+  //   title: 'Moodboard',
+  //   descr: 'Demo версия старой работы на React(+ добавил минимальный функционал)',
+  //   stack: 'React',
+  //   project: 'https://rainbowfieldholograph.github.io/moodboard-old-work/',
+  //   github: 'https://github.com/rainbowfieldholograph/moodboard-old-work',
+  // },
+  // {
+  //   image: reactsneakersImg,
+  //   title: 'React sneakers',
+  //   descr:
+  //     'Приложение, сделанное по видеокурсу react sneakers от archakov. Исправлено много багов в изначальном коде (до сих пор много багов, по-тихоньку исправляю)',
+  //   stack: 'React, MockAPI',
+  //   project: 'https://rainbowfieldholograph.github.io/react-sneakers/',
+  //   github: 'https://github.com/rainbowfieldholograph/react-sneakers',
+  // },
   {
     image: waterTransportation,
     title: 'Water Transportation',
