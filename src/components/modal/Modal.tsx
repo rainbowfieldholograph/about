f2dedcbd2f233d7696bb6104205276186e137270
rainbowfieldholograph <somebleed@tutanota.com>
2022-03-17 13:19:24 +0900

    modal accessability fix

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,71 +1,50 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { createPortal } from 'react-dom';
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
-import { FC, KeyboardEvent, useEffect, useRef } from 'react';
+import { FC, useEffect } from 'react';
 import styles from './Modal.module.css';
 import { ModalProps } from './Modal.props';
+import FocusTrap from 'focus-trap-react';
 
 const portalRootElement = document.getElementById('modal');
 
 export const Modal: FC<ModalProps> = ({ className, children, open, onClose }): JSX.Element => {
-  const contentRef = useRef<HTMLDivElement | null>(null);
-
-  const keyDownHandler = (event: KeyboardEvent) => {
-    if (event.key !== 'Tab') return;
-
-    const focusableModalElements = contentRef.current?.querySelectorAll(
-      'a[href], button:not([disabled]), textarea, input, select'
-    );
-
-    if (!focusableModalElements) return;
-
-    const firstElement = focusableModalElements[0] as HTMLElement;
-    const lastElement = focusableModalElements[
-      focusableModalElements.length - 1
-    ] as HTMLElement;
-    if (!event.shiftKey && document.activeElement === lastElement) {
-      firstElement.focus();
-      return event.preventDefault();
-    }
-    if (event.shiftKey && document.activeElement === firstElement) {
-      lastElement.focus();
-      event.preventDefault();
-    }
-  };
-
-  const focusContentOnOpen = () => {
-    const focusableEls = contentRef.current?.querySelectorAll(
-      'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), [tabindex="0"]'
-    );
-    if (!focusableEls) return;
-    const firstEl = Array.prototype.slice.call(focusableEls)[0];
-    firstEl?.focus();
+  const onClickEsc = (event: KeyboardEvent) => {
+    if (event.key === 'Esc' || event.key === 'Escape') onClose();
   };
 
   useEffect(() => {
-    if (open) focusContentOnOpen();
+    document.body.classList.add(styles._lock);
+  }, []);
+
+  useEffect(() => {
+    document.body.classList.toggle(styles._lock);
+    if (open) {
+      window.addEventListener('keydown', onClickEsc);
+      return () => window.removeEventListener('keydown', onClickEsc);
+    }
   }, [open]);
 
   if (!open || !portalRootElement) return <></>;
 
   return createPortal(
-    <>
-      <div onClick={onClose} className={styles.overlay} />
-      <div
-        ref={contentRef}
-        role="dialog"
-        tabIndex={-1}
-        className={[styles.modal, className].join(' ')}
-        onKeyDown={keyDownHandler}
-      >
-        <div className={styles.closeButtonWrapper}>
-          <button className={styles.closeButton} onClick={onClose}>
-            <FontAwesomeIcon icon={faTimesCircle} />
-          </button>
+    <FocusTrap>
+      <div onClick={onClose} className={styles.overlay}>
+        <div
+          role="dialog"
+          tabIndex={-1}
+          className={[styles.modal, className].join(' ')}
+          onClick={(event) => event.stopPropagation()}
+        >
+          <div className={styles.closeButtonWrapper}>
+            <button className={styles.closeButton} onClick={onClose}>
+              <FontAwesomeIcon icon={faTimesCircle} />
+            </button>
+          </div>
+          {children}
         </div>
-        {children}
       </div>
-    </>,
+    </FocusTrap>,
     portalRootElement
   );
 };
