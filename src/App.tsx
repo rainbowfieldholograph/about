0c24c830fe74ceee9fbb8431d8171a4e97cb8949
cherry <82304659+rainbowfieldholograph@users.noreply.github.com>
2022-04-13 23:45:33 +0900

    added github head link

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { Footer } from './sections/footer/Footer'
-import { MyWorks } from './sections/myWorks/MyWorks'
-import { Skills } from './sections/skills/Skills'
-import { Welcome } from './sections/welcome/Welcome'
-import './App.css'
-import './globals.css'
+import { Footer } from './sections/footer/Footer';
+import { MyWorks } from './sections/myWorks/MyWorks';
+import { Skills } from './sections/skills/Skills';
+import { Welcome } from './sections/welcome/Welcome';
+import './App.css';
+import './globals.css';
+import { GithubHeadLink } from './components/githubHeadLink/GithubHeadLink';
 
 const App = (): JSX.Element => (
   <div className="container">
+    <GithubHeadLink />
     <Welcome />
     <main className="contentSection">
       <Skills />
@@ -14,6 +16,6 @@ const App = (): JSX.Element => (
     </main>
     <Footer />
   </div>
-)
+);
 
-export default App
+export default App;
