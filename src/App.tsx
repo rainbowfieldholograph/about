e43f4df2757ec050d034c62452827f589e94435b
cherry <82304659+rainbowfieldholograph@users.noreply.github.com>
2022-04-14 23:07:53 +0900

    fix layout bug / change App styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import { Footer } from './sections/footer/Footer';
 import { MyWorks } from './sections/myWorks/MyWorks';
 import { Skills } from './sections/skills/Skills';
 import { Welcome } from './sections/welcome/Welcome';
-import './App.css';
+// import './App.css';
+import styles from './App.module.css';
 import './globals.css';
 import { GithubHeadLink } from './components/githubHeadLink/GithubHeadLink';
 
 const App = (): JSX.Element => (
-  <div className="container">
+  <div className={styles.container}>
     <GithubHeadLink />
-    <main>
+    <main className={styles.main}>
       <Welcome />
-      <div className="contentSection">
+      <div className={styles.contentSection}>
         <Skills />
         <MyWorks />
       </div>
